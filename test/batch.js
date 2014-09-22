4c4561e957492fac0888f4a89022c8ad5141416a
Adam Bretz <arbretz@gmail.com>
2014-09-22 14:31:54 -0400

    Cleaned up tests.

--- test/batch.js
+++ test/batch.js
@@ -16,7 +16,6 @@ var internals = {};
 
 var expect = Lab.expect;
 var before = Lab.before;
-var after = Lab.after;
 var describe = Lab.experiment;
 var it = Lab.test;
 
--- test/plugin.js
+++ test/plugin.js
@@ -13,8 +13,6 @@ var internals = {};
 // Test shortcuts
 
 var expect = Lab.expect;
-var before = Lab.before;
-var after = Lab.after;
 var describe = Lab.experiment;
 var it = Lab.test;
 
